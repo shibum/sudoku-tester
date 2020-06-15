fbe0a9a77e89a36b94d7407f6e56d68168547454
Shibu Michael <shibu@trellisys.net>
2020-06-15 16:16:38 +0530

    added htaccess file

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,11 @@
 <?php $this->start('body'); ?>
-<h1 class="text-center red">Lets start Sudoku from here..</h1>
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+    <div class="col-md-5 p-lg-5 mx-auto my-5">
+        <h1 class="display-4 font-weight-normal">Sudoku tester</h1>
+        <p class="lead font-weight-normal">Alright, consuming so much time to test your Sudoku answer? No worries! Let's test it in less than a minute.</p>
+        <a class="btn btn-outline-secondary" href="#">START SUDOKU TEST</a>
+    </div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+</div>
 <?php $this->end(); ?>
