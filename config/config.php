9034b80298ca1ba1effa3f4b340d0f3ad32b1ae0
Shibu Michael <shibu@trellisys.net>
2020-06-16 02:54:16 +0530

    config fix for production

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DEBUG' , true);
+define('DEBUG' , false);
 
 define('DEFAULT_CONTROLLER', 'Home'); // default controller if there isn't any.
 define('DEFAULT_LAYOUT', 'default'); // if no layout is set in the controller, then use this layout.
 
-define('PROOT', '/sudoku-tester/'); // set this to '/' for live server
+define('PROOT', '/'); // set this to '/' for live server
 define('SITE_TITLE', 'Sudoku Tester'); // use it if no site title is set.
